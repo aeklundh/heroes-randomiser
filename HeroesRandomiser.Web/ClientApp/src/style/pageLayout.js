20d93ab6a85251f23a08c1c6f3df80a41086ff57
aeklundh <eklundh.albin@gmail.com>
2018-11-05 21:21:33 +0100

    add gradient overlay to background image

--- HeroesRandomiser.Web/ClientApp/src/style/pageLayout.js
+++ HeroesRandomiser.Web/ClientApp/src/style/pageLayout.js
@@ -9,10 +9,22 @@ export const Viewport = withRouter(styled.div`
     flex-direction: column;
     width: 100%;
     min-height: 100vh;
-    background-image: url(https://prismic-io.s3.amazonaws.com/heroesrandomiser%2Fa88e7961-10b7-4ec1-9650-d0ec52f04106_gabriel-garcia-marengo-6132-unsplash-shrunk-1920.jpg);
+    background-image: url('https://prismic-io.s3.amazonaws.com/heroesrandomiser%2Fa88e7961-10b7-4ec1-9650-d0ec52f04106_gabriel-garcia-marengo-6132-unsplash-shrunk-1920.jpg');
     background-size: cover;
     background-position: center;
     font-family: ${props => props.theme.fonts.main};
+
+    &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 0;
+        background-image: linear-gradient(to bottom right, #6f00ff, #5600e0);;
+        opacity: 0.05;
+    }
 
     h1, h2, h3, h4, h5, h6 {
         font-family: ${props => props.theme.fonts.heading};
@@ -31,6 +43,7 @@ export const MainBody = withRouter(styled.main`
     width: 80%;
     background-color: ${props => props.theme.colours.mainBase};
     color: ${props => props.theme.colours.mainBaseText};
+    z-index: 1;
 
     ${media.desktop`
         width: 60%;
