c18a2690319cbd33c93c03ad964c0c71cad443fc
aeklundh <eklundh.albin@gmail.com>
2018-11-08 21:10:23 +0100

    add basic style to team container list

--- HeroesRandomiser.Web/ClientApp/src/components/TeamContainer.jsx
+++ HeroesRandomiser.Web/ClientApp/src/components/TeamContainer.jsx
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+//Style
+import styled from 'styled-components';
+import { media } from '../style/_mediaTemplates';
+
 //Actions
 import { fetchRandomTeam } from '../store/team/actions';
 
 //Utilities
 import { shouldFetchStandardReducable } from '../utilities/genericApiUtilities';
+
+const HeroList = styled.ul`
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    padding: 0;
+
+    ${media.desktop`
+        justify-content: space-between;
+    `}
+
+    li {
+        display: inline-block;
+        background-color: #b7b7b7;
+        margin-bottom: 20px;
+        padding: 1em;
+        box-shadow: 0px 0px 5px 0px #292828;
+        min-width: 125px;
+        text-align: center;
+    }
+`;
 
 class TeamContainer extends Component {
     componentDidMount = () => {
@@ -19,11 +46,11 @@ class TeamContainer extends Component {
         const { team } = this.props.team;
         return (
             <section>
-                <div>
+                <HeroList>
                     {team.map(hero => {
-                        return <p key={hero.id}>{hero.name}</p>
+                        return <li key={hero.id}>{hero.name}</li>
                     })}
-                </div>
+                </HeroList>
             </section>
         );
     }
