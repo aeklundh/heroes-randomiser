4272ab9181a7c006a0c20be7ed7f02fc465c9e2a
aeklundh <eklundh.albin@gmail.com>
2018-11-19 22:08:47 +0100

    fix PrismicCacheProvider cache duration policies on master refs and items

--- HeroesRandomiser.Web/Caching/PrismicCacheProvider.cs
+++ HeroesRandomiser.Web/Caching/PrismicCacheProvider.cs
@@ -15,13 +15,14 @@ namespace HeroesRandomiser.Web.Caching
 
         internal PrismicRef GetMasterRef() => _cacheProvider.TryGetCacheItem<PrismicRef>(CacheKeys.MasterRef);
 
-        internal void SetMasterRef(PrismicRef prismicRef) => _cacheProvider.SetCacheItem(CacheKeys.MasterRef, prismicRef);
+        internal void SetMasterRef(PrismicRef prismicRef) =>
+            _cacheProvider.SetCacheItem(CacheKeys.MasterRef, prismicRef, new MemoryCacheEntryOptions() { AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(10) });
 
         internal T TryGetPrismicCacheItem<T>(PrismicRef prismicRef, string cacheKeyBase) =>
             _cacheProvider.TryGetCacheItem<T>(FormatCacheKey(prismicRef, cacheKeyBase));
 
         internal void SetPrismicCacheItem<T>(PrismicRef prismicRef, string cacheKeyBase, T item) =>
-            _cacheProvider.SetCacheItem(FormatCacheKey(prismicRef, cacheKeyBase), item, new MemoryCacheEntryOptions() { AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(1) });
+            _cacheProvider.SetCacheItem(FormatCacheKey(prismicRef, cacheKeyBase), item, new MemoryCacheEntryOptions() { SlidingExpiration = TimeSpan.FromHours(3) });
 
         private string FormatCacheKey(PrismicRef prismicRef, string cacheKeyBase) => $"{prismicRef.Ref}|{cacheKeyBase}";
     }
