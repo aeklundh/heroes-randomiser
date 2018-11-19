8f773be4df04be7e8c77be33e6cd14b7d53ebe82
aeklundh <eklundh.albin@gmail.com>
2018-11-19 22:08:53 +0100

    add prismic webhook endpoint for refreshing master ref on publish

---
+++ HeroesRandomiser.Prismic/DataTransferObjects/PrismicWebhookDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeroesRandomiser.Prismic.DataTransferObjects
+{
+    public class PrismicWebhookDto
+    {
+        public string Type { get; set; }
+
+        [Required]
+        public string Secret { get; set; }
+
+        [Required]
+        public string MasterRef { get; set; }
+
+        public string Domain { get; set; }
+
+        public string ApiUrl { get; set; }
+    }
+}
---
+++ HeroesRandomiser.Web/Controllers/WebhooksController.cs
@@ -0,0 +1,52 @@
+﻿using HeroesRandomiser.Prismic;
+using HeroesRandomiser.Prismic.DataTransferObjects;
+using HeroesRandomiser.Web.Caching;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace HeroesRandomiser.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhooksController : ControllerBase
+    {
+        private readonly string _prismicWebhookSecretKey;
+        private readonly PrismicCacheProvider _cacheProvider;
+
+        public WebhooksController(IConfiguration configuration, PrismicCacheProvider cacheProvider)
+        {
+            _prismicWebhookSecretKey = configuration.GetValue<string>("Prismic:WebhookSecretKey");
+            _cacheProvider = cacheProvider;
+        }
+
+        [HttpPost]
+        [Route("prismic")]
+        public IActionResult OnPrismicPublish(PrismicWebhookDto dto)
+        {
+            if (string.IsNullOrEmpty(_prismicWebhookSecretKey))
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+
+            if (ModelState.IsValid)
+            {
+                if (_prismicWebhookSecretKey.Equals(dto.Secret, StringComparison.Ordinal))
+                {
+                    _cacheProvider.SetMasterRef(new PrismicRef()
+                    {
+                        Id = "master",
+                        Ref = dto.MasterRef,
+                        Label = "Master",
+                        IsMasterRef = true
+                    });
+
+                    return Ok();
+                }
+
+                return Unauthorized();
+            }
+
+            return BadRequest();
+        }
+    }
+}
